fda60a7d2c1cf0c41549315a605c1500371dd5b3
Daniel Agbeni <99095841+DanielAgbeni@users.noreply.github.com>
2024-10-08 17:53:40 +0100

    Built Hero section of About Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "custom-green": "#042A1D", // Add your custom color
-        "another-green": "#31B779", // green color for the 'REGISTER' word in the event section
-      },
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'custom-green': '#042A1D', // Add your custom color
+				'another-green': '#31B779', // green color for the 'REGISTER' word in the event section
+				'mountain-green': '##D3F8E1', //green for about hero page
+			},
 
-      lineHeight: {
-        70: "70px",
-      },
+			lineHeight: {
+				70: '70px',
+			},
 
-      fontFamily: {
-        pop: ["Poppins", "sans-serif"],
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+			fontFamily: {
+				pop: ['Poppins', 'sans-serif'],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
