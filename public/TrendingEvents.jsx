b33fd4af62732059afba5ff19e7dfc6303f8d175
Gautam Manchandani <161146829+GautamBytes@users.noreply.github.com>
2024-10-13 21:07:45 +0530

    Add files via upload

---
+++ public/TrendingEvents.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Card from './Card';
+import eventplaceholder from '../assets/eventplaceholder.jpeg'
+
+const TrendingEvents = () => {
+  const cards = [
+    {
+      title: 'EKITI HACKATHON 2023',
+      date: 'Monday, Oct 30, 4 pm',
+      link: '#',
+      imageSrc: eventplaceholder
+    },
+    {
+      title: 'EKITI HACKATHON 2023',
+      date: 'Monday, Oct 30, 4 pm',
+      link: '#',
+      imageSrc: eventplaceholder
+    },
+    {
+      title: 'EKITI HACKATHON 2023',
+      date: 'Monday, Oct 30, 4 pm',
+      link: '#',
+      imageSrc: eventplaceholder
+    },
+    {
+      title: 'EKITI HACKATHON 2023',
+      date: 'Monday, Oct 30, 4 pm',
+      link: '#',
+      imageSrc: eventplaceholder
+    },
+  ];
+
+  return (
+    <div className="py-12 bg-gray-50">
+      <h2 className="text-3xl font-bold text-center mb-8 text-[#0F513D]">Trending Events</h2>
+      <div className="flex gap-6 justify-center flex-wrap mx-auto max-w-6xl">
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            imageSrc={card.imageSrc}
+            title={card.title}
+            date={card.date}
+            link={card.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TrendingEvents;
