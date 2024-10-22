bf1f1605621b6c3be474143d3ebc5d7a2349d8a7
abidoyejay@gmail.com <abidoyejay@gmail.com>
2024-10-22 21:08:10 +0100

    wip:working on the AboutEvent page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import About from "./pages/About/About";
 import View from "./pages/View";
 import Event from "./pages/Event";
+import AboutEvent from "./pages/infosession/AboutEvent";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/view" element={<View />} />
         <Route path="/events" element={<Event />} />
+        <Route path="/AboutEvent" element={<AboutEvent />} />
       </Routes>
     </div>
   );
---
+++ src/pages/InfoSession/infosession.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AboutEvent from "../../pages/infosession/AboutEvent";
+import Footer from "../../components/Footer"
+import Header from '../../components/Navbar'
+
+const infosession = () => {
+  return (
+    <div>
+      <Header />
+      <AboutEvent />
+      <Footer />
+    </div>
+  );
+};
+
+export default infosession;
