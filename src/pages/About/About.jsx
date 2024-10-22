41c950a7c8c565a12925b0f8966914f792f3fe73
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2024-10-23 01:50:09 +0530

    Update back to top function

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+import { motion, useAnimation } from "framer-motion";
 import PreviousPost from './PreviousPost';
 import EventList from './UpcomingEvent';
 import Footer from '../../components/Footer';
 import Header from '../../components/Navbar';
 import AboutHero from '../../components/AboutHero';
 const About = () => {
+	const scrollControls = useAnimation();
+	const [showButton, setShowButton] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+		if (window.scrollY > 100) {
+			setShowButton(true);
+		} else {
+			setShowButton(false);
+		}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
+	const scrollToTop = () => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	};
 	return (
 		<div>
 			<Header />
@@ -12,6 +33,14 @@ const About = () => {
 			<EventList />
 			<PreviousPost />
 			<Footer />
+			{showButton && (
+				<button
+				onClick={scrollToTop}
+				className="fixed bottom-4 right-4 bg-blue-500 text-white p-2 rounded-full shadow-lg"
+				>
+				<FaArrowUp />
+				</button>
+			)}
 		</div>
 	);
 };
--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
 import Header from "../components/Navbar";
 import TrendingEvents from "../components/TrendingEvents";
 import RegisterEvents from "../components/RegisterEvents";
@@ -6,8 +7,27 @@ import NewsLetter from "../components/Newsletter";
 import Footer from "../components/Footer";
 
 const Event = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen mt-10 flex flex-col">
       <Header />
       <main className="flex-grow">
         <RegisterEvents />
@@ -15,6 +35,15 @@ const Event = () => {
         <NewsLetter />
       </main>
       <Footer />
+
+      {showButton && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-4 right-4 bg-blue-500 text-white p-2 rounded-full shadow-lg"
+        >
+          <FaArrowUp />
+        </button>
+      )}
     </div>
   );
 };
